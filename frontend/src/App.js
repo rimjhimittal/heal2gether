0b8eb72b3cce17df1bb0db28d4179050616fe890
Harsh <hjain_be21@thapar.edu>
2023-04-15 23:28:42 +0530

    fixed routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Router, Routes } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
-import home from './pages/home/home'
+// import home from './pages/home/home'
+import Home from './pages/home/home'
 import Forums from './pages/forums/forums';
-import events from './pages/events/events.js';
+import Events from './pages/events/events.js';
 // import OnlineTherapy from './pages/OnlineTherapy';
 
 function App() {
   return (
     <div className="App"><div className='App-header'>
       <Navbar />
-       <Router>
-      <Routes>
-        <Route exact path="/home" element={home} />
-        <Route path="/forums" element={Forums} />
-        <Route path="/events" element={events} />
+       <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/forums" element={<Forums/>} />
+        <Route path="/events" element={<Events/>} />
       </Routes>
-  </Router> 
     </div>
     </div>
   );
--- frontend/src/pages/events/events.js
+++ frontend/src/pages/events/events.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../events/events.css'
 
 function events() {
   return (
-    <div>
-      
+    <div id="bkl">
+      <h1>RIMJHIM BHEN KI LAUDI</h1>
     </div>
   )
 }
